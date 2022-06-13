5365f40fc205221b097d3480229ea9aed24dc1d3
Robert Doyer <tkdoyer@gmail.com>
2022-06-12 22:42:51 -0500

    unfinished stuck

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,38 @@
 var weatherData;
+var fivedayforcast;
 
  function handleSubmit(zipCode) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?units=imperial&zip=${zipCode}&appid=108b6dff15ff7287239749bc83d3a12f`)
     .then(res => res.json())
     .then(data => weatherData = data)
-    .then(() => buildCurrentWeather());
+    .then(() => buildCurrentWeather()).then(() => fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${weatherData.coord.lat}&lon=${weatherData.coord.lon}&exclude=current,minutely,alerts,hourly&appid=108b6dff15ff7287239749bc83d3a12f`))
+    .then(res => res.json())
+    .then(data => fivedayforcast = data)
+    .then(() => buildFiveDay());
 
+    if (!zipCode) {
+      alert(`Enter Zipcode`);
+      return;
+    }
+ }
+
+ function buildFiveDay(){
+   var fivedayforcastBoxs = document.getElementsByClassName(`fiveDay`)
+   
+  
+console.log("pay attention to me: ", fivedayforcastBoxs);
+var iconArray = fivedayforcast.daily.map(day => day.weather[0].icon);
+
+
+
+for(var i = 0; i < fivedayforcastBoxs.length;i++) {
+   fivedayforcastBoxs[i].children[1].src =`http://openweathermap.org/img/wn/${iconArray[i]}@2x.png`
+   // fivedayforcastBoxs[i].{target }.innerHTML =  fivedayforcast.daily.weather.temp; 
+
+}
+
+
+    
  }
 
  function buildCurrentWeather(){
